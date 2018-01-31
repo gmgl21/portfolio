61938d9caa3f9a29e3994a2815dc767dc28b3445
Gloria Lee <gmgl21@hotmail.com>
2018-01-31 01:19:17 +0000

    remove aria-controls from button

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,14 +1,8 @@
 const navButton = document.querySelector('button.navbar[aria-expanded]');
-const portfolioButton = document.querySelector('.portfolio');
 
 function toggleNav({target}) {
   const expanded = target.getAttribute('aria-expanded') === 'true' || false;
   navButton.setAttribute('aria-expanded',!expanded);
 }
 
-function portfolioList() {
-  portfolioButton.classList.toggle('open');
-}
-
 navButton.addEventListener('click', toggleNav);
-portfolioButton.addEventListener('click', portfolioList);
