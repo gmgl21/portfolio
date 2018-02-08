544f69916ce46bb0d037aa17c92e941418e9a462
Gloria Lee <gmgl21@hotmail.com>
2018-02-08 01:36:44 +0000

    fix nav, increase font-size for contact on mobile devices

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,8 +1,18 @@
 const navButton = document.querySelector('button.navbar[aria-expanded]');
+const menuList = document.querySelector('.menu-list');
 
 function toggleNav({target}) {
   const expanded = target.getAttribute('aria-expanded') === 'true' || false;
   navButton.setAttribute('aria-expanded',!expanded);
 }
 
+function collapseNav() {
+  navButton.setAttribute('aria-expanded', false);
+}
+
 navButton.addEventListener('click', toggleNav);
+menuList.addEventListener('click', collapseNav);
+
+
+
+
