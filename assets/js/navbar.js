8235239f64e3d5219f1dfb4addfcc8a7f4b8a697
Gloria Lee <gmgl21@hotmail.com>
2018-02-09 02:53:01 +0000

    make navbar scrollable, change arrow-down icon

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,6 @@
 const navButton = document.querySelector('button.navbar[aria-expanded]');
 const menuList = document.querySelector('.menu-list');
+const showMenuItem = document.querySelector('a.menu-item');
 
 function toggleNav({target}) {
   const expanded = target.getAttribute('aria-expanded') === 'true' || false;
@@ -10,6 +11,11 @@ function collapseNav() {
   navButton.setAttribute('aria-expanded', false);
 }
 
+function showItems() {
+  document.querySelector('ul').classList.toggle('expand');
+}
+
+showMenuItem.addEventListener('mouseover', showItems);
 navButton.addEventListener('click', toggleNav);
 menuList.addEventListener('click', collapseNav);
 
